33073217c79e2c23bc90c025d5bf861fa19ce8ce
App Academy Student <student@appacademy.io>
2014-07-16 10:17:22 -0400

    fix bullets

--- game.js
+++ game.js
@@ -1,70 +1,68 @@
 (function(root) {
   var Asteroids = root.Asteroids = (root.Asteroids || {});
-  
+
   var Game = Asteroids.Game = function(ctx) {
     this.asteroids = [];
     this.ship = new Asteroids.Ship();
     this.bullets = [];
-        
+
     this.ctx = ctx;
     this.xDim = Game.DIM_X;
     this.yDim = Game.DIM_Y;
-    for (var i = 0; i < 0; i++) {
+    for (var i = 0; i < 50 ; i++) {
       this.asteroids.push(new Asteroids.Asteroid());
     };
   };
-  
+
   Game.prototype.movingObjects = function () {
     return this.asteroids.concat(this.bullets).concat([this.ship])
   };
-  
+
   Game.DIM_X = 1000;
   Game.DIM_Y = 500;
-  
+
   Game.prototype.draw = function() {
     var ctx = this.ctx;
     ctx.clearRect(0, 0, this.xDim, this.yDim);
-    
+
     this.movingObjects().forEach(function(movingObj){
       movingObj.draw(ctx);
     });
   };
-  
+
   Game.prototype.move = function () {
     this.movingObjects().forEach(function(movingObj){
       movingObj.move();
     });
   };
-  
-  Game.prototype.step = function () {    
+
+  Game.prototype.step = function () {
     if (key.isPressed("up")) {
       this.ship.vel = this.ship.vel.sum(
         Asteroids.Vector.fromPolar(this.ship.angle, 0.01));
     }
-    
+
     if (key.isPressed("down")) {
       this.ship.vel = this.ship.vel.sum(
         Asteroids.Vector.fromPolar(this.ship.angle, -0.01));
     }
-    
+
     if (key.isPressed("left")) {
+
       this.ship.angle -= .01;
     }
-    
+
     if (key.isPressed("right")) {
       this.ship.angle += .01;
     }
-    
+
     var game = this;
-    
-    key("space", function() {
-      game.bullets.push(new Asteroids.Bullet(game.ship));
-    });
-    
+
     this.move();
     this.draw();
+
   };
-  
+
   Game.prototype.checkCollisions = function (myVar) {
     var game = this;
     this.asteroids.forEach(function(astr, i){
@@ -79,20 +77,24 @@
       })
     })
   };
-  
+
   Game.prototype.stop = function (myVar) {
     clearInterval(myVar);
   };
-  
+
   Game.prototype.start = function () {
     var game = this;
+    key("space", function() {
+        game.bullets.push(new Asteroids.Bullet(game.ship));
+     });
+
     var inter = window.setInterval(function () {gameFunc()}, 10);
-    
-    gameFunc = function () {
+
+    var gameFunc = function () {
       game.step();
       game.checkCollisions(inter);
     };
-    
-    
+
+
   };
 }(this));
