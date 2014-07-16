9471f03f12d1ca8bc2b5e778b976d4b82c12b7ed
Jorge <jamedranoa@unal.edu.co>
2014-07-16 16:19:28 -0400

    mod out bullet's position by game dimensions

--- bullet.js
+++ bullet.js
@@ -1,15 +1,23 @@
 (function(root) {
   var Asteroids = root.Asteroids = (root.Asteroids || {});
-  
+
   var Bullet = Asteroids.Bullet = function(ship) {
     var vel = ship.vel.sum(Asteroids.Vector.fromPolar(ship.angle, Bullet.SPEED));
-    
-    Asteroids.MovingObject.call(this, ship.pos, vel, Bullet.RADIUS, Bullet.COLOR);
+
+    Asteroids.MovingObject.call(
+      this,
+      new Asteroids.Vector(
+        ship.pos.x % Asteroids.Game.DIM_X,
+        ship.pos.y % Asteroids.Game.DIM_Y),
+      vel,
+      Bullet.RADIUS,
+      Bullet.COLOR
+    );
   };
-  
+
   Bullet.RADIUS = 3;
   Bullet.COLOR = "black";
   Bullet.SPEED = 3;
-  
+
   Asteroids.Bullet.inherits(Asteroids.MovingObject);
 }(this));
